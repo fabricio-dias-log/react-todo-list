4884cd23bc5b49cfa37033a126a9100d264b9d8c
fabricio-intersoft <148587991+fabricio-intersoft@users.noreply.github.com>
2024-02-19 15:13:24 -0300

    Form and functions to add tasks is done

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 // Components
 import Header from './components/Header';
 import Footer from './components/Footer';
 import TaskForm from './components/TaskForm';
 import TaskList from './components/TaskList';
 
+// Interfaces
+import { ITask } from './interfaces/Task';
+
+// CSS
 import styles from './App.module.css';
 
 function App() {
+  const [tasksList, setTasksList] = useState<ITask[]>([]);
+
   return (
     <div>
       <Header/>
       <main className={styles.main}>
       <div>
         <h2>O que você vai fazer?</h2>
-        <TaskForm btnText='Criar Tarefa'/>
+        <TaskForm btnText='Criar Tarefa' taskList={tasksList} setTaskList={setTasksList}/>
       </div>
       <div>
         <h2>Suas Tarefas:</h2>
