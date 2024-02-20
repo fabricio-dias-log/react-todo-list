3d4052833f3d760cc8a4eb29a752a75afda99504
Fabricio Dias <fabricio270909hotmail.com>
2024-02-19 22:09:48 -0300

    General style

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -6,8 +6,8 @@ interface Props {}
 const Modal = (props: Props) => {
   return (
     <div id="modal">
-        <div></div>
-        <div>
+        <div className={styles.fade}></div>
+        <div className={styles.modal}>
             <h2>Texto modal</h2>
         </div>
     </div>
